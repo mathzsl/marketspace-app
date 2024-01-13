a2edb4c38529c4918213dd75ef99218bf2df3453
mathzsl <maths.soares99@gmail.com>
2024-01-13 03:31:41 -0300

    feat: PAYMENT_METHODS data

---
+++ mobile/src/data/paymentMethods.ts
@@ -0,0 +1,7 @@
+export const PAYMENT_METHODS = [
+  { value: 'boleto', name: 'Boleto' },
+  { value: 'pix', name: 'Pix' },
+  { value: 'cash', name: 'Dinheiro' },
+  { value: 'card', name: 'Cartão de crédito' },
+  { value: 'deposit', name: 'Depósito bancário' },
+]
