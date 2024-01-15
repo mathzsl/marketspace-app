f0348b578a45cda77a04fca0a5eb4a7568422405
mathzsl <maths.soares99@gmail.com>
2024-01-15 14:59:56 -0300

    feat: adding new route Edit Ad

--- mobile/src/routes/app.routes.tsx
+++ mobile/src/routes/app.routes.tsx
@@ -8,6 +8,7 @@ import { CreateAd } from '@screens/CreateAd'
 import { AdDetails } from '@screens/AdDetails'
 import { MyAdDetails } from '@screens/MyAdDetails'
 import { MyAdPreview } from '@screens/MyAdPreview'
+import { EditAd } from '@screens/EditAd'
 
 type AppRoutesProps = {
   home: undefined
@@ -15,6 +16,7 @@ type AppRoutesProps = {
   myAdDetails: undefined
   myAdPreview: undefined
   createAd: undefined
+  editAd: undefined
 }
 
 export type AppNavigatorRoutesProps = NativeStackNavigationProp<AppRoutesProps>
@@ -27,13 +29,14 @@ export function AppRoutes() {
       screenOptions={{
         headerShown: false,
       }}
-      initialRouteName="createAd"
+      initialRouteName="editAd"
     >
       <Screen name="home" component={TabRoutes} />
       <Screen name="adDetails" component={AdDetails} />
       <Screen name="myAdDetails" component={MyAdDetails} />
       <Screen name="myAdPreview" component={MyAdPreview} />
       <Screen name="createAd" component={CreateAd} />
+      <Screen name="editAd" component={EditAd} />
     </Navigator>
   )
 }
