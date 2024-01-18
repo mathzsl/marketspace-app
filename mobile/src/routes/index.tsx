2ee64600dfe2252c13c5f5bfd1460cf4d90938f1
mathzsl <maths.soares99@gmail.com>
2024-01-17 22:27:44 -0300

    feat: added Toast provider

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -1,12 +1,16 @@
 import { NavigationContainer } from '@react-navigation/native'
 
+import Toast from 'react-native-toast-message'
+
 import { AppRoutes } from './app.routes'
 import { AuthRoutes } from './auth.routes'
 
 export function Routes() {
   return (
     <NavigationContainer>
-      <AppRoutes />
+      <AuthRoutes />
+
+      <Toast />
     </NavigationContainer>
   )
 }
