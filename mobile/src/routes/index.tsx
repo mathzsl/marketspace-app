21145acc52c72da87f1c28ca2231bcd1a3b3385f
mathzsl <maths.soares99@gmail.com>
2024-01-18 17:57:40 -0300

    feat: added loading component to routes

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -4,12 +4,20 @@ import Toast from 'react-native-toast-message'
 
 import { AppRoutes } from './app.routes'
 import { AuthRoutes } from './auth.routes'
+import { useAuth } from '@hooks/useAuth'
+
+import { Loading } from '@components/Loading'
 
 export function Routes() {
+  const { user, isLoadingUserStorageData } = useAuth()
+
+  if (isLoadingUserStorageData) {
+    return <Loading />
+  }
+
   return (
     <NavigationContainer>
-      <AuthRoutes />
-
+      {user.id ? <AppRoutes /> : <AuthRoutes />}
       <Toast />
     </NavigationContainer>
   )
