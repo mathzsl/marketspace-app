c98c16390df10e6095033f004a99a4cb1c58e404
mathzsl <maths.soares99@gmail.com>
2024-01-19 06:03:32 -0300

    feat: Switch component

--- mobile/src/components/Switch/index.tsx
+++ mobile/src/components/Switch/index.tsx
@@ -1,13 +1,28 @@
+import { useEffect, useState } from 'react'
 import {
   Switch as NativeSwitch,
   SwitchProps as NativeSwitchProps,
 } from 'react-native'
+
 import { useTheme } from 'styled-components/native'
 
-type SwitchProps = NativeSwitchProps
+type SwitchProps = NativeSwitchProps & {
+  value?: boolean
+  onValueChange: (value: boolean) => void
+}
 
-export function Switch({ ...rest }: SwitchProps) {
+export function Switch({ value, onValueChange, ...rest }: SwitchProps) {
+  const [switchValue, setSwitchValue] = useState(value ?? false)
+
   const { colors } = useTheme()
+
+  function switchToggle() {
+    setSwitchValue((prevState) => !prevState)
+  }
+
+  useEffect(() => {
+    onValueChange(switchValue)
+  }, [switchValue])
 
   return (
     <NativeSwitch
@@ -17,6 +32,8 @@ export function Switch({ ...rest }: SwitchProps) {
       }}
       trackColor={{ false: colors.gray_300, true: colors.blue_300 }}
       thumbColor={colors.gray_100}
+      onChange={switchToggle}
+      value={switchValue}
       ios_backgroundColor={colors.gray_300}
       {...rest}
     />
