7d4a15890a833dbfd3e5aa17b93a7accdd6e7548
mathzsl <maths.soares99@gmail.com>
2024-01-20 18:46:04 -0300

    feat: product filter functionality

--- mobile/src/screens/MyAds/index.tsx
+++ mobile/src/screens/MyAds/index.tsx
@@ -1,3 +1,4 @@
+import { useCallback, useState } from 'react'
 import { Dimensions, FlatList, TouchableOpacity, View } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
@@ -10,12 +11,24 @@ import { Picker } from '@components/Picker'
 import { Text } from '@components/Typography'
 import { AdListCard } from '@components/AdListCard'
 
-import { useNavigation } from '@react-navigation/native'
+import { useFocusEffect, useNavigation } from '@react-navigation/native'
+
 import { AppNavigatorRoutesProps } from '@routes/app.routes'
+
+import { api } from '@services/api'
+
+import { ProductDTO } from '@dtos/ProductDTO'
+import { Loading } from '@components/Loading'
+import { AppError } from '@utils/AppError'
+import Toast from 'react-native-toast-message'
 
 const LIST_ITEM_SIZE = Math.floor(Dimensions.get('screen').width - 68) / 2
 
 export function MyAds() {
+  const [myProducts, setMyProducts] = useState<ProductDTO[]>([])
+  const [filteredProducts, setFilteredProducts] = useState<ProductDTO[]>([])
+  const [productIsLoading, setProductIsLoading] = useState(true)
+
   const navigation = useNavigation<AppNavigatorRoutesProps>()
 
   const insets = useSafeAreaInsets()
@@ -23,6 +36,49 @@ export function MyAds() {
 
   function handleCreateNewAd() {
     navigation.navigate('createAd')
+  }
+
+  function showAdsByFilter(filter: string) {
+    if (filter === 'active') {
+      return setFilteredProducts(myProducts.filter((ad) => ad.is_active))
+    }
+
+    if (filter === 'inactive') {
+      return setFilteredProducts(myProducts.filter((ad) => !ad.is_active))
+    }
+
+    if (filter === 'all') {
+      return setFilteredProducts(myProducts)
+    }
+  }
+
+  async function fetchMyProducts() {
+    try {
+      setProductIsLoading(true)
+
+      const { data } = await api.get('users/products')
+
+      setMyProducts(data)
+    } catch (error) {
+      const isAppError = error instanceof AppError
+      const title = isAppError
+        ? error.message
+        : 'Não foi possível carrgar a sua lista de produtos.'
+
+      Toast.show({ text1: title, position: 'top', type: 'error' })
+    } finally {
+      setProductIsLoading(false)
+    }
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchMyProducts()
+    }, []),
+  )
+
+  if (productIsLoading) {
+    return <Loading />
   }
 
   return (
@@ -44,11 +100,19 @@ export function MyAds() {
             numberOfLines={2}
             style={{ flex: 1 }}
           >
-            9 anúncios
+            {myProducts.length}{' '}
+            {`${myProducts.length !== 1 ? 'anúncios' : 'anúncio'}`}
           </Text>
 
           <View style={{ width: 112 }}>
-            <Picker />
+            <Picker
+              options={[
+                { label: 'Todos', value: 'all' },
+                { label: 'Ativos', value: 'active' },
+                { label: 'Inativos', value: 'inactive' },
+              ]}
+              onValueChange={showAdsByFilter}
+            />
           </View>
         </InfoBox>
 
@@ -58,12 +122,19 @@ export function MyAds() {
           columnWrapperStyle={{
             gap: 20,
           }}
-          data={[1, 2, 4, 6, 7]}
-          keyExtractor={(item) => String(item)}
+          data={filteredProducts}
+          keyExtractor={(item) => item.id}
           contentContainerStyle={{ paddingBottom: 50, gap: 32 }}
-          renderItem={() => (
+          renderItem={({ item }) => (
             <View style={{ width: LIST_ITEM_SIZE }}>
-              <AdListCard onPress={() => console.log('fi')} />
+              <AdListCard
+                withAvatar={false}
+                isActive={item.is_active}
+                data={item}
+                onPress={() =>
+                  navigation.navigate('myAdDetails', { productId: item.id })
+                }
+              />
             </View>
           )}
         />
