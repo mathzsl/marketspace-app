cc4400107ce524ddacd46e9b308fc5b26f67aebd
mathzsl <maths.soares99@gmail.com>
2024-01-20 07:12:54 -0300

    fix: typing adjustments

--- mobile/src/components/ImageSlider/index.tsx
+++ mobile/src/components/ImageSlider/index.tsx
@@ -1,18 +1,27 @@
 import { useState } from 'react'
 import { Dimensions, Image, View } from 'react-native'
-import Carousel from 'react-native-reanimated-carousel'
+
 import { Message, Overlay, Step, Steps } from './styles'
+
+import Carousel from 'react-native-reanimated-carousel'
+import { api } from '@services/api'
 
 type ImageSliderProps = {
   isDisabled?: boolean
+  formatImage?: boolean
   images: string[]
 }
 
-export function ImageSlider({ images, isDisabled = false }: ImageSliderProps) {
+export function ImageSlider({
+  images,
+  formatImage = false,
+  isDisabled = false,
+}: ImageSliderProps) {
   const [currentIndex, setCurrentIndex] = useState(0)
 
   const width = Dimensions.get('window').width
   const height = Dimensions.get('window').height
+
   return (
     <Overlay>
       {isDisabled && <Message>Anúncio desativado</Message>}
@@ -21,20 +30,24 @@ export function ImageSlider({ images, isDisabled = false }: ImageSliderProps) {
         <Carousel
           loop={false}
           width={width}
-          height={height / 3}
+          height={height / 2.5}
           data={images}
           scrollAnimationDuration={1000}
           onProgressChange={(_, absoluteProgress) =>
             setCurrentIndex(Math.round(absoluteProgress + 1))
           }
-          renderItem={(item) => (
+          renderItem={({ item }) => (
             <View
               style={{
                 flex: 1,
               }}
             >
               <Image
-                source={{ uri: item.item }}
+                source={{
+                  uri: !formatImage
+                    ? item
+                    : `${api.defaults.baseURL}/images/${item}`,
+                }}
                 style={{
                   width: '100%',
                   height: '100%',
