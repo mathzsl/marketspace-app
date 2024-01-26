0b9ed90fcf3c7383d138eca86edcb2bcf300d56f
mathzsl <maths.soares99@gmail.com>
2024-01-26 17:08:49 -0300

    fix: adjustment in typing

--- mobile/src/dtos/FiltersDTO.ts
+++ mobile/src/dtos/FiltersDTO.ts
@@ -1,6 +1,6 @@
 export type FiltersDTO = {
-  is_new: boolean | undefined
-  accept_trade: boolean | undefined
-  payment_methods: string[]
-  query: string | undefined
+  is_new: string | undefined
+  accept_trade: string | undefined
+  payment_methods: { key: string; name: string }[] | undefined
+  query?: string | undefined
 }
