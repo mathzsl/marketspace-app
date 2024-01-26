0345740139b7ef755ac0eb08bd2cdc2da2adbaad
mathzsl <maths.soares99@gmail.com>
2024-01-26 17:05:57 -0300

    fix: added filter function

--- mobile/src/components/FormFilter/index.tsx
+++ mobile/src/components/FormFilter/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Box, Container, Footer, Label, TagInputBox } from './styles'
 
 import { Button } from '@components/Button'
@@ -6,46 +7,105 @@ import { Checkbox } from '@components/Checkbox'
 import { TagInput } from '@components/TagInput'
 
 import { PAYMENT_METHODS } from '../../data/paymentMethods'
+import { FiltersDTO } from '@dtos/FiltersDTO'
 
-export function FormFilter() {
+type ValueProps = {
+  key: string
+  name: string
+}
+
+type FormFilterProps = {
+  value?: FiltersDTO
+  onChange: () => Promise<void>
+  onAddingFilter: (filters: FiltersDTO) => void
+}
+
+export function FormFilter({
+  value,
+  onChange,
+  onAddingFilter,
+}: FormFilterProps) {
+  const [is_new, setIs_new] = useState<string | undefined>(
+    value?.is_new ?? undefined,
+  )
+  const [accept_trade, setAccept_trade] = useState<string | undefined>(
+    value?.accept_trade ?? undefined,
+  )
+  const [payment_methods, setPayment_methods] = useState<
+    ValueProps[] | undefined
+  >(value?.payment_methods ?? undefined)
+
+  function handleResetFilters() {
+    setIs_new(undefined)
+    setAccept_trade(undefined)
+    setPayment_methods(undefined)
+
+    onChange()
+  }
+
+  async function handleAplyFilters() {
+    const formattedFilters: FiltersDTO = {
+      is_new,
+      accept_trade,
+      payment_methods,
+    }
+
+    onAddingFilter(formattedFilters)
+  }
+
   return (
     <Container>
       <Box>
         <Label>Condição</Label>
         <TagInputBox>
           <TagInput
-            isActive
+            isActive={is_new === 'true'}
             label="NOVO"
-            onChange={() => console.log('onChange')}
-            onRemove={() => console.log('onRemove')}
+            onChange={() => setIs_new('true')}
+            onRemove={() => setIs_new(undefined)}
           />
 
           <TagInput
-            isActive={false}
+            isActive={is_new === 'false'}
             label="USADO"
-            onChange={() => console.log('onChange')}
-            onRemove={() => console.log('onRemove')}
+            onChange={() => setIs_new('false')}
+            onRemove={() => setIs_new(undefined)}
           />
         </TagInputBox>
       </Box>
 
       <Box>
         <Label>Aceita troca?</Label>
-        <Switch />
+        <Switch
+          value={accept_trade === 'true'}
+          onChange={() =>
+            setAccept_trade(accept_trade === 'true' ? 'false' : 'true')
+          }
+        />
       </Box>
 
       <Box style={{ marginBottom: 'auto' }}>
         <Label>Meios de pagamentos aceitos</Label>
-        <Checkbox options={PAYMENT_METHODS} />
+        <Checkbox
+          value={payment_methods}
+          onChange={(value) => setPayment_methods(value)}
+          options={PAYMENT_METHODS}
+        />
       </Box>
 
       <Footer>
-        <Button title="Resetar filtros" variant="primary" style={{ flex: 1 }} />
+        <Button
+          title="Resetar filtros"
+          variant="primary"
+          style={{ flex: 1 }}
+          onPress={handleResetFilters}
+        />
 
         <Button
           title="Aplicar filtros"
           variant="secondary"
           style={{ flex: 1 }}
+          onPress={handleAplyFilters}
         />
       </Footer>
     </Container>
