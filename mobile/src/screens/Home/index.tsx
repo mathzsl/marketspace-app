70d03814cb36601577866b4712e8442da1b658c9
mathzsl <maths.soares99@gmail.com>
2024-01-28 02:39:05 -0300

    feat: finished Home screen

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,10 +1,12 @@
-import { useRef } from 'react'
+import { useCallback, useRef, useState } from 'react'
 import { FlatList, Dimensions, View } from 'react-native'
 
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 import { Container, Label } from './styles'
 
+import { Text } from '@components/Typography'
+import { Loading } from '@components/Loading'
 import { HomeHeader } from '@components/HomeHeader'
 import { AdListCard } from '@components/AdListCard'
 import { BottomSheet } from '@components/BottomSheet'
@@ -14,17 +16,124 @@ import { MyAdsCardButton } from '@components/MyAdsCardButton'
 import { BottomSheetModal } from '@gorhom/bottom-sheet'
 import { FormFilter } from '@components/FormFilter'
 
-import { useNavigation } from '@react-navigation/native'
+import { useFocusEffect, useNavigation } from '@react-navigation/native'
 import { AppNavigatorRoutesProps } from '@routes/app.routes'
+
+import { api } from '@services/api'
+
+import { ProductDTO } from '@dtos/ProductDTO'
+import { AppError } from '@utils/AppError'
+
+import Toast from 'react-native-toast-message'
+import { FiltersDTO } from '@dtos/FiltersDTO'
 
 const LIST_ITEM_SIZE = Math.floor(Dimensions.get('screen').width - 68) / 2
 
 export function Home() {
+  const [isLoading, setIsLoading] = useState(true)
+  const [productList, setProductList] = useState<ProductDTO[]>([])
+  const [filters, setFilters] = useState<FiltersDTO>({} as FiltersDTO)
+  const [myActiveProducts, setMyActiveProducts] = useState(0)
+
   const insets = useSafeAreaInsets()
   const paddingTop = insets.top
 
   const ref = useRef<BottomSheetModal>(null)
   const navigation = useNavigation<AppNavigatorRoutesProps>()
+
+  async function fetchMyActiveProducts() {
+    try {
+      const { data } = await api.get('users/products')
+
+      setMyActiveProducts(data.length)
+    } catch (error) {
+      const isAppError = error instanceof AppError
+      const title = isAppError
+        ? error.message
+        : 'Não foi possível carregar a quantidade de produtos ativos. Tente novamente mais tarde!'
+
+      Toast.show({
+        text1: title,
+        position: 'top',
+        type: 'error',
+      })
+    }
+  }
+
+  async function fetchProducts() {
+    try {
+      setIsLoading(true)
+
+      if (filters) {
+        setFilters({} as FiltersDTO)
+      }
+
+      const { data } = await api.get('/products')
+
+      setProductList(data)
+    } catch (error) {
+      const isAppError = error instanceof AppError
+      const title = isAppError
+        ? error.message
+        : 'Não foi possível carregar a lista de produtos. Tente novamente mais tarde!'
+
+      Toast.show({
+        text1: title,
+        position: 'top',
+        type: 'error',
+      })
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  async function handleAplyFilter(filter: FiltersDTO) {
+    try {
+      setIsLoading(true)
+
+      const filtersApplied = {
+        ...filter,
+        payment_methods: filter.payment_methods?.map((method) => method.key),
+        query: filters.query,
+      }
+
+      setFilters(filter)
+
+      ref.current?.close()
+
+      const { data } = await api.get('/products', {
+        params: {
+          ...filtersApplied,
+        },
+      })
+
+      setProductList(data)
+    } catch (error) {
+      const isAppError = error instanceof AppError
+      const title = isAppError
+        ? error.message
+        : 'Não foi possível carregar a lista de produtos. Tente novamente mais tarde!'
+
+      Toast.show({
+        text1: title,
+        position: 'top',
+        type: 'error',
+      })
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchMyActiveProducts()
+      fetchProducts()
+    }, []),
+  )
+
+  if (isLoading) {
+    return <Loading />
+  }
 
   return (
     <Container style={{ paddingTop }}>
@@ -35,9 +144,9 @@ export function Home() {
           marginTop: 24,
           gap: 20,
         }}
-        data={[1, 2, 4, 6, 7, 8, 9, 10]}
-        keyExtractor={(item) => String(item)}
-        ListHeaderComponent={() => (
+        data={productList}
+        keyExtractor={(item) => item.id}
+        ListHeaderComponent={
           <>
             <HomeHeader />
 
@@ -45,27 +154,54 @@ export function Home() {
               Seus produtos anunciados para venda
             </Label>
 
-            <MyAdsCardButton numberOfAds={5} />
+            <MyAdsCardButton activeAds={myActiveProducts} />
 
             <Label style={{ marginTop: 32 }}>Compre produtos variados</Label>
 
             <SearchInput
+              value={filters.query}
+              onChangeText={(text) => setFilters({ ...filters, query: text })}
               placeholder="Buscar anúncio"
-              onPressSearch={() => console.log('onPressSearch pressionado')}
+              onPressSearch={() => handleAplyFilter(filters)}
               onPressFilter={() => ref.current?.present()}
             />
           </>
-        )}
-        contentContainerStyle={{ paddingBottom: 50 }}
-        renderItem={() => (
+        }
+        contentContainerStyle={[
+          !productList.length && { flex: 1 },
+          { paddingBottom: 50 },
+        ]}
+        renderItem={({ item }) => (
           <View style={{ width: LIST_ITEM_SIZE }}>
-            <AdListCard onPress={() => navigation.navigate('adDetails')} />
+            <AdListCard
+              data={item}
+              onPress={() =>
+                navigation.navigate('adDetails', { productId: item.id })
+              }
+            />
           </View>
         )}
+        ListEmptyComponent={
+          <View
+            style={{
+              flex: 1,
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            <Text color="gray_400" style={{ textAlign: 'center' }}>
+              A lista encontra-se vazia; =(
+            </Text>
+          </View>
+        }
       />
 
       <BottomSheet bottomSheetModalRef={ref} title="Filtrar anúncios">
-        <FormFilter />
+        <FormFilter
+          value={filters}
+          onAddingFilter={handleAplyFilter}
+          onChange={fetchProducts}
+        />
       </BottomSheet>
     </Container>
   )
